917616c9e561d3aeaa7297b9d8c6fcae16da18ca
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-12 08:15:23 +0700

    learn about prototype in object

--- section-14-OOP/script.js
+++ section-14-OOP/script.js
@@ -4,11 +4,22 @@
 
 // actually use function, but have a different with function normal
 
-const Person = function (firstName, lastName) {
+const Person = function (firstName, age) {
   this.firstName = firstName;
-  this.lastName = lastName;
+  this.age = age;
 };
 
-const robi = new Person('Robi', 'Nurhidayat');
+const robi = new Person('Robi', 23);
 
+Person.prototype.calcAge = function () {
+  console.log(30 - this.age);
+};
+
+robi.calcAge();
+
+Person.prototype.status = 'Mahasiswa';
+
+console.log(robi.hasOwnProperty(status));
+
+console.log(Person.prototype);
 console.log(robi);
