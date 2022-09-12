1d2519a0ee61dc5be31adc89c3e8b286d1d338ee
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-12 20:01:35 +0700

    finishing create mini project random color bg

--- practice/Mini-project/Random-Color-Bg/index.js
+++ practice/Mini-project/Random-Color-Bg/index.js
@@ -1,11 +1,17 @@
 const button = document.querySelector(".btn");
 
-let changeColor = false;
-let color;
-
 button.addEventListener("click", function () {
-  changeColor ? (color = "lightblue") : (color = "salmon");
+  const red = Math.trunc(Math.random() * 255) + 1;
+  const green = Math.trunc(Math.random() * 255) + 1;
+  const blue = Math.trunc(Math.random() * 255) + 1;
+  const alfa = String(Math.random()).slice(0, 4);
+  const color = `rgba(${red},${green},${blue},${alfa})`;
+
+  console.log(color);
+  console.log(alfa);
   document.body.style.backgroundColor = color;
 
-  changeColor = !changeColor;
+  document.querySelector(".value").textContent = getComputedStyle(
+    document.body
+  ).backgroundColor;
 });
