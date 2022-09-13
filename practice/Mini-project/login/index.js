1d602db03ead8d6c0dd03c6739e775fd56db320f
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-13 20:12:43 +0700

    add logic in js for mini project login

--- practice/Mini-project/login/index.js
+++ practice/Mini-project/login/index.js
@@ -1,10 +1,35 @@
 const labels = document.querySelectorAll("label");
+const button = document.querySelector(".btn");
+const errorUsername = document.querySelector(".error__username");
+const errorPassword = document.querySelector(".error__password");
+const inputUsername = document.querySelector(".input__username");
+const inputPassword = document.querySelector("input[type=password]");
+const iconUsername = document.querySelector(".icon__username");
+const iconPassword = document.querySelector(".icon__password");
 labels.forEach((label) => label.classList.add("opacity"));
 
-const inputs = document.querySelectorAll("input");
+button.addEventListener("click", function (e) {
+  e.preventDefault();
 
-inputs.forEach((input, i) => {
-  input.addEventListener("change", function () {
-    labels[i].style.opacity = 100;
-  });
+  if (inputPassword.value.length < 5 && inputPassword.value.length < 5) {
+    errorUsername.textContent = `Characters must more than 8`;
+    errorPassword.textContent = "Password should more than 8";
+    iconPassword.textContent = "X";
+    iconUsername.textContent = "X";
+  } else {
+    inputUsername.value = "";
+    inputPassword.value = "";
+
+    alert("Loading...");
+  }
+});
+
+iconUsername.addEventListener("click", function () {
+  errorUsername.style.display = "none";
+  iconUsername.style.display = "none";
+});
+
+iconPassword.addEventListener("click", function () {
+  errorPassword.style.display = "none";
+  iconPassword.style.display = "none";
 });
