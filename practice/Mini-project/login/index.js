60a665798aca87b86824178894bd6644281d602c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-13 07:06:19 +0700

    styling input and create logic js

--- practice/Mini-project/login/index.js
+++ practice/Mini-project/login/index.js
@@ -1,2 +1,10 @@
 const labels = document.querySelectorAll("label");
 labels.forEach((label) => label.classList.add("opacity"));
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input, i) => {
+  input.addEventListener("change", function () {
+    labels[i].style.opacity = 100;
+  });
+});
