ded87f52d9760a868a81f14a5ad4c8060648a724
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-27 18:43:01 +0700

    melanjutkan latihan membuat dropdown menu

--- practice/Mini-project/dropdown-menu/index.js
+++ practice/Mini-project/dropdown-menu/index.js
@@ -1,8 +1,18 @@
 const currentMenu = document.querySelector(".current--menu");
 const icon = document.querySelector(".icon");
 const listMenu = document.querySelector(".list__menu");
+const currItem = document.querySelector(".current--item");
+
+const allListMenu = document.querySelector(".list__menu");
 
 currentMenu.addEventListener("click", function () {
   icon.classList.toggle("icon--rotate");
   listMenu.classList.toggle("hidden");
+});
+
+allListMenu.addEventListener("click", function (e) {
+  const item = e.target;
+  if (!item.classList.contains("list--item")) return;
+
+  currItem.textContent = item.textContent;
 });
