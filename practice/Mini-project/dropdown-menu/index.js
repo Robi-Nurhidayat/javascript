48f0c157561174b8155ad6936906adc507179729
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-09-28 18:52:05 +0700

    mencoba lagi latihan membuat dropdown menu

--- practice/Mini-project/dropdown-menu/index.js
+++ practice/Mini-project/dropdown-menu/index.js
@@ -1,13 +1,31 @@
+"use strict";
 const currentMenu = document.querySelector(".current--menu");
 const icon = document.querySelector(".icon");
 const listMenu = document.querySelector(".list__menu");
 const currItem = document.querySelector(".current--item");
-
 const allListMenu = document.querySelector(".list__menu");
+
+const menu = [
+  {
+    label: "Facebook",
+  },
+  {
+    label: "Twitter",
+  },
+  {
+    label: "Instagram",
+  },
+];
 
 currentMenu.addEventListener("click", function () {
   icon.classList.toggle("icon--rotate");
   listMenu.classList.toggle("hidden");
+});
+
+menu.forEach((m) => {
+  const html = `<span class="list--item">${m.label}</span>`;
+
+  allListMenu.insertAdjacentHTML("beforeend", html);
 });
 
 allListMenu.addEventListener("click", function (e) {
