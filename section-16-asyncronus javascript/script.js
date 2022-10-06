7df0709cf74deef6e35098eb66b100a9f6caaf2e
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-06 10:06:44 +0700

    finishing challenge 1 section 16 javascript

--- section-16-asyncronus javascript/script.js
+++ section-16-asyncronus javascript/script.js
@@ -41,30 +41,66 @@ const getCountryNeighbor = function (data) {
 
 // getCountry('indonesia');
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+//   // countriesContainer.style.opacity = 1;
+// };
 
-const getCountry = function (country) {
-  fetch(`https:restcountries.com/v3.1/name/${country}`)
+// const getCountry = function (country) {
+//   fetch(`https:restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`country not found status ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       getCountryNeighbor(data[0]);
+
+//       return fetch(`https:restcountries.com/v3.1/name/${country}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => getCountryNeighbor(data[0], 'neighbor'))
+//     .catch(err => {
+//       console.log(err);
+//       renderError(`Gagal memuat ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountry('indonesia');
+// });
+
+// getCountry('asdasd');
+
+// Challenge 1
+
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://geocode.xyz/${lat},${lng},14z?geoit=json&auth=3591584854395899404x59258`
+  )
     .then(response => {
       if (!response.ok) {
-        throw new Error(`country not found status ${response.status}`);
+        throw new Error(`err ${response.status}`);
       }
       return response.json();
     })
     .then(data => {
+      return fetch(`https:restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
       console.log(data);
       getCountryNeighbor(data[0]);
-
-      return fetch(`https:restcountries.com/v3.1/name/${country}`);
     })
-    .then(response => response.json())
-    .then(data => getCountryNeighbor(data[0], 'neighbor'))
     .catch(err => {
       console.log(err);
-      renderError(`Gagal memuat ${err.message}`);
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
@@ -72,7 +108,5 @@ const getCountry = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountry('indonesia');
+  whereAmI(-6.9351854, 107.6002968);
 });
-
-getCountry('asdasd');
