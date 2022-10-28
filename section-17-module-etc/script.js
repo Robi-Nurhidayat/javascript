28987b1926235156df7daedb870e036c03c92f93
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-10-28 07:09:31 +0700

    belajar import semua nya dengan menggunakan *

--- section-17-module-etc/script.js
+++ section-17-module-etc/script.js
@@ -1,7 +1,11 @@
-import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
-console.log('import module');
+// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+// console.log('import module');
 
-addToCart('Bread', 100);
+// addToCart('Bread', 100);
 
-console.log(totalPrice);
-console.log(totalQuantity);
+// console.log(totalPrice);
+// console.log(totalQuantity);
+
+import * as ShoppingCart from './shoppingCart.js';
+
+ShoppingCart.addToCart('bread', 2000);
